a66d379d2a9348df9fee7ae6c8d5445ba6678152
kishore <kishore.kumar@m8y.one>
2021-12-18 20:05:41 +0530

    react Redux BoilerPlate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./redux/countReducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const { count } = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
+
+	const handleIncrement = (e) => {
+		e.preventDefault();
+		dispatch(increment());
+	};
+
+	const handleDecrement = (e) => {
+		e.preventDefault();
+		dispatch(decrement());
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h1>React Redux Toolkit</h1>
+				<p>This is a boilerplate for a React Redux application.</p>
+				<hr />
+				<h3>Counter</h3>
+				<p>Count: {count}</p>
+				<div>
+					<button onClick={handleIncrement}>Increment</button>
+					<button onClick={handleDecrement}>Decrement</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
